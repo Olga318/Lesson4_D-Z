34b0efb1d543159f2fa7652fb00ed3d39b35a1be
Kosyachenko Olga <kosya4enko.o@yandex.ru>
2022-09-14 22:18:17 +0300

    52

--- seminar7/Program.cs
+++ seminar7/Program.cs
@@ -50,9 +50,9 @@ int[,] array = CreateRandomTwoDemArray(m,n,min,max);*/
 // Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение 
 //этого элемента или же указание, что такого элемента нет.
 
-//
+//не решена
 
-int[,]CreateRandomTwoDemArray(int m, int n,  int min, int max)
+/*int[,]CreateRandomTwoDemArray(int m, int n,  int min, int max)
 {
     int[,] newMatrix = new int[m, n];
     for(int i= 0; i<m; i++)
@@ -90,16 +90,62 @@ if (int[2,3]==22)
 }
 else{
     Console.WriteLine("Элемента А на данной позиции нет");
+}*/
+
+
+
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+/*
+Console.Clear();
+Console.Write("Введите количество строк m:  ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество  n:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[m, n];
+FillArrayRandomNambers(numbers);
+PrintArray(numbers);
+Console.WriteLine();
+
+for(int j =0; j< numbers.GetLength(1); j++)
+{
+    double avarage =0;
+    for(int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = ( avarage + numbers[i,j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage +"; ");
+}
+Console.WriteLine();
+
+void FillArrayRandomNambers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
 }
 
-
-
-
-
-
-
-
-
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] +"\t");
+        }
+        Console.WriteLine("");
+    }
+}
+Console.ReadLine();*/
 
 
 
@@ -160,3 +206,17 @@ void PrintArray(int[,] array )
         Console.WriteLine("");
     }
 }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
